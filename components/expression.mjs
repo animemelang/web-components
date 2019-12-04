e0c0070a3b9b1ad27c62d0cf971e25c27055b5e1
bucket3432 <bucket3432@outlook.com>
2019-12-04 09:21:15 -0500

    Add anm-expression component
    This is a container component used simply to contain an expression.

---
+++ components/expression.mjs
@@ -0,0 +1,9 @@
+/**
+ * The Animeme expression element.
+ * This element serves as a container for Animeme expressions.
+ *
+ * This element is functionally equivalent to an HTMLSpanElement.
+ */
+export default class AnimemeExpressionElement extends HTMLElement {}
+
+window.customElements.define("anm-expression", AnimemeExpressionElement);
--- components/index.mjs
+++ components/index.mjs
@@ -1,2 +1,3 @@
 export { default as AnimemeProgramElement } from "./program.mjs";
 export { default as AnimemeSayElement } from "./say.mjs";
+export { default as AnimemeExpressionElement } from "./expression.mjs";
