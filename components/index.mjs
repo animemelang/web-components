e8dcc0e843f8910ae41686ba7590a17c375e4131
bucket3432 <bucket3432@outlook.com>
2019-12-04 20:14:28 -0500

    Add anm-test component
    This component represents a test whose result can be checked for later.
    
    The meme is "Is this a pigeon?".

--- components/index.mjs
+++ components/index.mjs
@@ -2,3 +2,4 @@ export { default as AnimemeProgramElement } from "./program.mjs";
 export { default as AnimemeSayElement } from "./say.mjs";
 export { default as AnimemeExpressionElement } from "./expression.mjs";
 export { default as AnimemeNoopElement } from "./noop.mjs";
+export { default as AnimemeTestElement } from "./test.mjs";
---
+++ components/test.mjs
@@ -0,0 +1,51 @@
+const template = document.createElement("template");
+
+/**
+ * The Animeme test condition statement.
+ * This statement compares a value against some condition.
+ *
+ * This component accepts two slots: "value" and "test".
+ * The value slot renders the value to compare.
+ * The test slot renders the condition.
+ */
+export default class AnimemeTestElement extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.appendChild(document.importNode(template.content, true));
+  }
+}
+
+template.innerHTML = `
+<style>
+  :host {
+    display: inline-block;
+    position: relative;
+    width: 853px;
+    height: 480px;
+  }
+
+  #value {
+    display: inline-block;
+    position: absolute;
+    top: 85px;
+    left: 645px;
+    transform: translate(-50%, -50%);
+    text-align: center;
+  }
+
+  #test-wrapper {
+    display: block;
+    position: absolute;
+    bottom: 22px;
+    text-align: center;
+    width: 100%;
+  }
+</style>
+<img src="../resources/statements/test/test.png" width="853" height="480" />
+<slot id="value" name="value"></slot>
+<div id="test-wrapper">Is this <slot id="test" name="test">a pigeon</slot>?</div>
+`;
+
+window.customElements.define("anm-test", AnimemeTestElement);
+
